67f5f8f73e21a53bb96a13734d2bf0e9b26ad4db
Kamin <Ultimatezim@yahoo.com>
2012-06-21 12:42:32 -0400

    Final Sprint 1 turn in... sorta
    Signed-off-by: Kamin <Ultimatezim@yahoo.com>

--- FallRiver/FallRiver/Emitter.cpp
+++ FallRiver/FallRiver/Emitter.cpp
@@ -226,7 +226,7 @@ void Emitter::Update(float fElapsedTime)
 void Emitter::Render() 
 {
 	ViewManager* view = ViewManager::GetInstance();
-	view->DrawUnfilledRect( this->rect, 255, 255, 255 );
+	//view->DrawUnfilledRect( this->rect, 255, 255, 255 );
 	for( unsigned int i = 0; i < _m_vparticles.size(); ++i)
 	{
 		_m_vparticles[i]->Render();
